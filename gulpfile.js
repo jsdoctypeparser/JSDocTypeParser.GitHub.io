95625d18c7f72a369fb6e52c0d3ba44eb830bc0a
Brett Zamir <brettz9@yahoo.com>
2019-06-11 21:22:10 +0800

    - Remove deprecated bower and dependencies in favor of npm (with a gulp copy routine to copy locally) - Use latest npm deps but avoid updating bootstrap beyond 3.x (whose design changed considerably on version 4)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,20 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
+var sourceFiles = [
+  'node_modules/bootstrap/dist/css/bootstrap.min.css',
+  'node_modules/bootstrap/dist/fonts/*',
+  'node_modules/github-fork-ribbon-css/gh-fork-ribbon.css',
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/google-code-prettify/src/prettify.js',
+];
+var outputPath = 'copied_node_modules/';
+
+gulp.task('copy', function () {
+  return gulp
+      .src(sourceFiles)
+      .pipe(gulp.dest(outputPath));
+});
 
 gulp.task('javascript', function () {
   var b = browserify({
@@ -19,4 +33,4 @@ gulp.task('javascript', function () {
 });
 
 
-gulp.task('build', gulp.series('javascript'));
+gulp.task('build', gulp.parallel('javascript', 'copy'));
