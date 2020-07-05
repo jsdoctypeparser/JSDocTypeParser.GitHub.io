02f64dad95556433d9b7b13338f777ba007e227a
Brett Zamir <brettz9@yahoo.com>
2020-07-05 10:38:02 +0800

    - Linting: As per latest ESLint (rules apply to hidden files by default now) - npm: Update dep. (jquery) and devDeps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
+'use strict';
+
 module.exports = {
-  "env": {
-    "node": true,
-    "browser": true,
-    "es6": true
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
   },
-  "extends": ["eslint:recommended"],
-  "rules": {
-    "prefer-const": [2],
-    "no-var": [2],
-    "quotes": [2, "single", "avoid-escape"],
-    "no-use-before-define": [2, "nofunc"],
-    "space-infix-ops": [2, { "int32Hint": false }],
-    "curly": [2, "multi-line"],
-    "comma-dangle": [2, "always-multiline"],
+  extends: ['eslint:recommended'],
+  rules: {
+    quotes: [2, 'single', 'avoid-escape'],
+    curly: [2, 'multi-line'],
+    'prefer-const': [2],
+    'no-var': [2],
+    'no-use-before-define': [2, 'nofunc'],
+    'space-infix-ops': [2, { 'int32Hint': false }],
+    'comma-dangle': [2, 'always-multiline'],
 
     // For Closure Library style parameter namings such as opt_optionalParam, var_args.
-    "camelcase": 0
-  }
+    camelcase: 0,
+  },
 };
